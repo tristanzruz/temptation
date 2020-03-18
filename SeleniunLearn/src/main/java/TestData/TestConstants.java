b27d93c89805b1d523fd1559942db3c28390670f
Tristan Alejandro TACZ. Cruz Zeferino <it9@original-group.com>
2020-03-18 08:40:35 -0500

    primeros

---
+++ SeleniunLearn/src/main/java/TestData/TestConstants.java
@@ -0,0 +1,16 @@
+package TestData;
+
+public class TestConstants {
+
+    public static final class TestData {
+
+        public static final String HOME_URL="https://pricetravel.com";
+
+
+    }
+
+    public static final class TestNGGroups{
+        public static final String SELENIUM = "selenium_tests";
+
+    }
+}
